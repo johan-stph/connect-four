6859c1d22464b0eaa16a9ddc83961b0e789d3491
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-08-14 12:56:42 +0200

    test changes

--- src/app/api/connect-four/route.ts
+++ src/app/api/connect-four/route.ts
@@ -2,6 +2,9 @@ import {NextRequest, NextResponse} from "next/server";
 import {analyze_position} from "@johan-stph/connect-four-solver";
 
 export async function GET(request: NextRequest) {
+    if (request.method !== "GET") {
+        return NextResponse.error()
+    }
     // Extract the 'position' parameter from the request's query string
     const position = request.nextUrl.searchParams.get("position") || "";
 
